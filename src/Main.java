dfae0b8a7a0b67101a34f7a5e3e3c5a4cc505e74
KozCodes <kozorajc23@gcc.edu>
2025-02-17 15:24:34 -0500

    Change 2

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hi, how are you?");
+        System.out.println("Not to bad you?");
+    }
+}
